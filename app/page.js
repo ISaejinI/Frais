669bf077ef129f4c5bd8278854dc07531ed11c3b
Lou-Anne <saejinmx@gmail.com>
2026-06-23 19:35:42 +0200

    feat: use link tag for links

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <a href="/dashboard">Aller sur le dashboard</a>
-        <a href="/sandbox">Aller sur le sandbox</a>
+        <link href="/dashboard">Aller sur le dashboard</link>
+        <link href="/sandbox">Aller sur le sandbox</link>
       </main>
   );
 }
