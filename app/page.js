e057ce5912a8989d8010793918e8725224ce774e
Lou-Anne <saejinmx@gmail.com>
2026-06-23 15:08:02 +0200

    test: deploy

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <p>Bienvenue sur l'application Frais!</p>
+        <a href="/dashboard">Aller sur le dashboard</a>
+        <a href="/sandbox">Aller sur le sandbox</a>
       </main>
   );
 }
