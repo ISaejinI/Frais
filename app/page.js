ddbe37bcd9e45fab35ac53734ce292daa4bff64f
Lou-Anne <saejinmx@gmail.com>
2026-06-24 09:22:10 +0200

    hotfix: link import issues

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <link href="/dashboard">Aller sur le dashboard</link>
-        <link href="/sandbox">Aller sur le sandbox</link>
+        <Link href="/dashboard">Aller sur le dashboard</Link>
+        <Link href="/sandbox">Aller sur le sandbox</Link>
       </main>
   );
 }
