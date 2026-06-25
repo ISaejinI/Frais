fb338707c405eb8592c911dc032825048b59d228
Lou-Anne <saejinmx@gmail.com>
2026-06-25 10:25:45 +0200

    feat: add real texts & colors

--- data/pet-states.js
+++ data/pet-states.js
@@ -1,53 +1,58 @@
 export const petStates = [
     {
         id: 0,
-        name: "Au top (Chill & Fraîche)",
+        name: "Pétillant",
         humor: "happy",
         imgUrl: "pet-chill.svg",
         descBeforeAction: "On frôle la perfection ! Mais attention, le soleil commence à taper sur les vitres. Vite, le premier qui voit un rayon de soleil baisse le store ! On ne lâche rien.",
         descAfterAction: "Non mais regarde-moi ce score parfait ! Je revis, je danse, je ne transpire même plus d'un pixel. Tu es officiellement mon humain préféré de la journée !",
         scoreMax: 100,
         scoreMin: 85,
+        color: "#00B8C4"
     },
     {
         id: 1,
-        name: "Soulagement / Récupération",
+        name: "Au frais",
         humor: "excited",
         imgUrl: "pet-recovering.svg",
         descBeforeAction: "Alerte ! On vient de perdre un petit degré de fraîcheur... C'est le début de la fin ! Bon, d'accord, j'exagère un peu, mais un petit coup de pouce pour fermer la fenêtre serait parfait.",
         descAfterAction: "Ouf ! Je sens un petit courant d'air frais qui me redonne vie ! On revient de loin, mes circuits commençaient à tiédir. Merci pour ce sauvetage en règle !",
         scoreMax: 84,
         scoreMin: 65,
+        color: "#00B9A8"
     },
     {
         id: 2,
-        name: "Vigilante (Ça chauffe)",
+        name: "Tranquille",
         humor: "confused",
         imgUrl: "pet-recovering.svg",
         descBeforeAction: "Oula, c'est moi ou l'ambiance devient tropicale ? On est en train de perdre la fraîcheur. Un petit coup d'œil aux conseils ?",
         descAfterAction: "Halte là, l'hémorragie est stoppée ! Le thermomètre arrête enfin de grimper. Ne relâchons pas l'effort !",
         scoreMax: 64,
         scoreMin: 45,
+        color: "#78B8AD"
     },
     {
         id: 3,
-        name: "En détresse (Inconfort chaud)",
+        name: "Un peu chaud",
         humor: "sad",
         imgUrl: "pet-distress.svg",
         descBeforeAction: "OK, je commence officiellement à bouillir... S'il te plaît, ferme ce store ou coupe une lumière, j'ai chaud !",
         descAfterAction: "C'est pas encore le pôle Nord, mais je revis ! Merci d'avoir agi, mes circuits reprennent des couleurs.",
         scoreMax: 44,
         scoreMin: 25,
+        color: "#F49E14"
     },
     {
         id: 4,
-        name: "Mode Sauna (Urgence / Mauvais choix)",
+        name: "En surchauffe",
         humor: "angry",
         imgUrl: "pet-distress.svg",
         descBeforeAction: "Alerte rouge ! Je suis un glaçon oublié sur le bitume. On ferme d'urgence les fenêtres et les volets avant qu'on ne fonde !",
         descAfterAction: "Merci... L'incendie thermique est éteint. On repart de loin alors on ne lâche rien, garde cette pièce bien close !",
         scoreMax: 24,
         scoreMin: 5,
+        color: "#F46725"
     },
     {
         id: 5,
@@ -58,5 +63,6 @@ export const petStates = [
         descAfterAction: "Gloups... Je me re-condense ! Merci pour cette bouffée d'air frais, j'ai bien failli finir en vapeur d'eau.",
         scoreMax: 4,
         scoreMin: 0,
+        color: "#F46725"
     }
 ]
